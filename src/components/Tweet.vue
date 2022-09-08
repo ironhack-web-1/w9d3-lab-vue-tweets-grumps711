7f5fe882a32cc5df043d17bf28b3ad7472bdbd4e
grumpycat711 <eugeni.puigdomenech@gmail.com>
2022-09-08 13:50:49 +0200

    Done

--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -9,13 +9,7 @@
 
       <Message :message="message" />
 
-      <div className="actions">
-        <!-- Font Awesome icons -->
-        <i class="far fa-comment"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="far fa-heart"></i>
-        <i class="fas fa-share"></i>
-      </div>
+      <Actions />
     </div>
 
     <i class="fas fa-ellipsis-h"></i>
